e6ac5370a97f1a05aeee332e06003ff67f997a84
Yuval Geller <User@LAB3-12>
2022-03-21 14:53:40 +0200

    FDFDF

---
+++ FootyPunditsApp/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,59 @@
+﻿using FootballDataApi.Models;
+using FootyPunditsApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace FootyPunditsApp.ViewModels
+{
+    class HomePageViewModel : BaseViewModel
+    {
+        public HomePageViewModel()
+        {
+            GetMatchesByDate();
+        }
+
+
+
+        private async void GetCompetitions()
+        {
+            FootballDataAPIProxy proxy = FootballDataAPIProxy.CreateProxy();
+            try
+            {
+                var competions = await proxy.Competition.GetAvailableCompetition();
+
+
+                if (competitions != null)
+                {
+                    this.Competitions.Clear();
+                    foreach (Competition c in competions)
+                    {
+                        Competitions.Add(c);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private async void GetMatchesByDate()
+        {
+            FootballDataAPIProxy proxy = FootballDataAPIProxy.CreateProxy();
+            var matches = await proxy.Match.GetAllMatchOfCompetition(2021);
+        }
+
+        private ObservableCollection<Competition> competitions;
+        public ObservableCollection<Competition> Competitions
+        {
+            get => competitions;
+            set
+            {
+                competitions = value;
+                OnPropertyChanged("Competitions");
+            }
+        }
+    }
+}
--- FootyPunditsApp/Views/GroupsView.xaml.cs
+++ FootyPunditsApp/Views/GroupsView.xaml.cs
@@ -3,19 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FootyPunditsApp.ViewModels;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace FootyPunditsApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class HomePageView : ContentView
+    public partial class GroupsView : ContentView
     {
-        public HomePageView()
+        public GroupsView()
         {
-            HomePageViewModel context = new HomePageViewModel();
-            this.BindingContext = context;
             InitializeComponent();
         }
     }
