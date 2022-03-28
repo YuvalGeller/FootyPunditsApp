bb3222465e65da39df7f1c7e314360de75ce904b
Yuval Geller <User@LAB3-12>
2022-03-28 15:58:00 +0300

    Added changes to profilePage

--- FootyPunditsApp/ViewModels/ProfilePageViewModel.cs
+++ FootyPunditsApp/ViewModels/ProfilePageViewModel.cs
@@ -1,20 +1,145 @@
-﻿using System;
+﻿using FootyPunditsApp.Models;
+using FootyPunditsApp.Services;
+using FootyPunditsApp.Views;
+using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows.Input;
 using Xamarin.Forms;
-using System.Threading.Tasks;
-using FootyPunditsApp.Services;
-using FootyPunditsApp.Models;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Xamarin.Essentials;
+
 
 
 namespace FootyPunditsApp.ViewModels
 {
-    class ProfilePageViewModel
+    class ProfilePageViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event Action<Page> Push;
+        private FootyPunditsAPIProxy proxy;
+        private FileResult imageFileResult;
+        public event Action<ImageSource> SetImageSourceEvent;
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (this.name != value)
+                {
+                    this.name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        private UserAccount user;
+        public UserAccount User
+        {
+            get
+            {
+                return this.user;
+            }
+            set
+            {
+                if (this.user != value)
+                {
+                    this.user = value;
+                    OnPropertyChanged(nameof(User));
+                }
+            }
+        }
+
+        private string profileImage;
+        public string ProfileImage
+        {
+            get
+            {
+                return this.profileImage;
+            }
+            set
+            {
+                if (this.profileImage != value)
+                {
+                    this.profileImage = value;
+                    OnPropertyChanged(nameof(ProfileImage));
+                }
+            }
+        }
+
+        private DateTime joinedAt;
+        public DateTime JoinedAt
+        {
+            get
+            {
+                return this.joinedAt;
+            }
+            set
+            {
+                if (this.joinedAt != value)
+                {
+                    this.joinedAt = value;
+                    OnPropertyChanged(nameof(JoinedAt));
+                }
+            }
+        }
+
+        public ProfilePageViewModel()
+        {
+            proxy = FootyPunditsAPIProxy.CreateProxy();
+            LoadProfile();
+        }
+
+        public void LoadProfile()
+        {
+            User = ((App)App.Current).CurrentUser;
+            
+            Name = $"{User.AccName}";
+            JoinedAt = User.SignUpDate.Date;
+            //ProfileImage = $"{proxy.baseUri}/imgs/{User.ProfileImage}";
+            
+        }
+
+        //public Command ChangePfpCommand => new Command(() => ChangePfp());
+        //public async void ChangePfp()
+        //{
+        //    if (MediaPicker.IsCaptureSupported)
+        //    {
+        //        FileResult result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions()
+        //        {
+        //            Title = "Pick a profile picture"
+        //        });
+
+        //        if (result != null)
+        //        {
+        //            this.imageFileResult = result;
+
+        //            var stream = await result.OpenReadAsync();
+        //            ImageSource imgSource = ImageSource.FromStream(() => stream);
+        //            if (SetImageSourceEvent != null)
+        //                SetImageSourceEvent(imgSource);
+        //            bool uploadImageSuccess = await proxy.UploadImage(imageFileResult.FullPath, $"a{User.Id}.jpg");
+        //            if (uploadImageSuccess)
+        //                User.ProfileImage = $"a{User.Id}.jpg";
+        //        }
+        //    }
+        //    else
+        //    {
+        //        // add error popup
+        //    }
     }
 }
